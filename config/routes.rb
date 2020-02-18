ca92a0cd64725f002630b5ebaeefa692b177ca1b
ft93 <franco.teguh@yahoo.com>
2020-02-18 14:58:01 +0700

    Initial commit, basic layout setup, and add basic user functionality

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+	authenticated :user do 
+		root to: 'dashboard#index', as: :authenticated_root
+	end
+  root to: redirect('/users/sign_in')
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
