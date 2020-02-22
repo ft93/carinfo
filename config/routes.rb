40be40d2336b5fa5be76061e0b43ba19a1ae13aa
ft93 <franco.teguh@yahoo.com>
2020-02-22 11:41:09 +0700

    Add registration user through admin account

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
 	authenticated :user do 
 		root to: 'dashboard#index', as: :authenticated_root
 	end
   root to: redirect('/users/sign_in')
   devise_for :users, controllers: {registrations: "registrations"}
-  
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
